35b679de0aab3da0fd7bd6dd63989f4a447ebb30
Benzi Darwin <ssalibenjamin0402@gmail.com>
2025-02-07 09:23:27 +0300

    Reduced docker size #2

--- pre-commit.js
+++ pre-commit.js
@@ -2,9 +2,7 @@ const { execSync } = require("child_process");
 
 try {
   console.log("Running Prettier...");
-  execSync('npx prettier --write . "--ignore-path .prettierignore"', {
-    stdio: "inherit",
-  });
+  execSync("npx prettier --write .", { stdio: "inherit" });
   execSync("git add -A", { stdio: "inherit" });
   console.log("Prettier formatting complete.");
 } catch (error) {
